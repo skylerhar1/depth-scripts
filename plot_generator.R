38c644ab96498438e81f892b8c40f2b662699bc6
skylerhar1 <hars@oregonstate.edu>
2022-08-09 14:16:30 -0700

    Finished single isolate trial, in working order

--- plot_generator.R
+++ plot_generator.R
@@ -9,7 +9,7 @@ ii_info <- read.table(file = "ii_info.txt", header = F, sep = "\t")
 colnames(ii_info) <- c("Isolate","Island")
 #READ IN THE DATA
 filename <- "B_127_con_L1_1.fq.gz_depth_depth.table"
-depth_table <- read.table(file=filename, header = F, sep = "\t")
+depth_table <- read.table(file=filename, header = T, sep = "\t")
 colnames(depth_table) <- c("CHROM","POS","DEPTH")
 split_name<-strsplit(filename,split = "_")
 #take the first two string chinks and uses that as the ID
@@ -19,20 +19,21 @@ island <- ii_info$Island[which(ii_info$Isolate == iso_id)]
 #REFLOW THE POSITION TO BE CONTINUOUS
 num_rows <- nrow(depth_table)
 depth_table$POS <- 1:num_rows
-
+head(depth_table)
 #GENOME COVERAGE AS A SLIDING WINDOW
-
 depth.average<-setDT(depth_table)[, .(
   window.start = rollapply(POS, width=10000, by=1000, FUN=min, align="left", partial=TRUE),
   window.end = rollapply(POS, width=10000, by=1000, FUN=max, align="left", partial=TRUE),
   coverage = rollapply(DEPTH, width=10000, by=1000, FUN=mean, align="left", partial=TRUE)
 ), .(CHROM)]
+head(depth.average)
+tail(depth.average)
 #PLOT THE DATA
 depth.average.plot <- ggplot(depth.average, aes(x=window.end, y=coverage, colour=CHROM)) + 
-  geom_point(shape = 20, size = 2, colour=depth.average$CHROM) +
+  geom_point(shape = 20, size = 0.7, colour=depth.average$CHROM) +
   geom_line(colour=depth.average$CHROM, size = 0.1)+
   scale_x_continuous(name="Genomic Position (bp)", limits=c(0,num_rows ),labels = scales::comma,breaks = scales::pretty_breaks(n = 20)) +
-  scale_y_continuous(name="Average Coverage Depth", limits=c(0,300))+
+  scale_y_continuous(name="Average Coverage Depth", limits=c(0,350))+
   ggtitle(iso_id, subtitle = island)
-plot_name <- gsub(" ","",paste("png_directory/",iso_id,".png"))
-ggsave(plot_name, width = 18000, height = 800, units = "px",limitsize = FALSE)
+plot_name <- gsub(" ","",paste("png_directory/",iso_id,"flagged.png"))
+ggsave(plot_name, width = 18000, height = 900, units = "px",limitsize = FALSE)
